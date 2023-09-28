b1400bb1e7f0ce7064dc86c08378239df0a11e1b
mumbereh <siirasonh@gmail.com>
2023-09-28 14:33:10 +0300

    modified queries.sql file

--- queries.sql
+++ queries.sql
@@ -8,3 +8,15 @@ SELECT name, escape_attempts FROM Animals WHERE weight_kg > 10.5;
 SELECT * FROM Animals WHERE neutered = TRUE;
 SELECT * FROM Animals WHERE name<> 'Gabumon';
 SELECT * FROM Animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+BEGIN;
+DELETE FROM Animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT weight_update;
+UPDATE Animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO weight_update;
+UPDATE Animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
